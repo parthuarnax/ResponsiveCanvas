beecbca92e96bcdd16e575e18b1312a3a3f6155a
FireCodingDay <69696645+parthuarnax@users.noreply.github.com>
2021-01-06 09:29:01 +0900

    response : window size / fullscreen(no browser tabs)

--- sketch.js
+++ sketch.js
@@ -1,27 +1,23 @@
 
-var w = window.innerWidth;
-var h = window.innerHeight;  
-let canvas;
-
 function setup() {
   //canvas=createCanvas(w, h);
-  fullscreen();
+  fullscreen(true);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255,255,128);
-  // A rectangle
   fill(200, 200, 200);
   noStroke();
-  textSize(100);
-  text("A",width/2,height/2);
-  rect(20, 20, w-40, h-40);
-  // uses global variables for width and height
-  }
+  fill(0);
+  rect(0,0,windowWidth,windowHeight);
+  textSize(20);
+  fill(255);
+  text(windowWidth,width/2,windowHeight/2);
+  text(windowHeight,widnowWidth/2,windowHeight/2+30);
+  //uses global variables for width and height
+ }
 
-window.onresize = function() {
-  // assigns new values for width and height variables
-  w = window.innerWidth;
-  h = window.innerHeight;  
-  canvas.size(w,h);
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
 }
